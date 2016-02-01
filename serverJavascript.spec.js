53e3e3dae22c91a38fb6bad3fc19de2a5d726fac
devlitz <z_wolfe@mac.com>
2016-02-01 14:41:34 -0600

    Added more testing for gpaCalc

--- serverJavascript.spec.js
+++ serverJavascript.spec.js
@@ -13,4 +13,17 @@ describe('testing gpaCalc', function(){
     it('We should get 4.000 for all A', function() {
         expect(gpaCalc(1, 1, 1, 4, 4, 4).toEqual(4.000));
     });
+
+    it('We should get 2.000 for all C', function() {
+        expect(gpaCalc(1, 1, 1, 2, 2, 2).toEqual(0.000));
+    });
+
+    it('We should get 0.000 for all F', function() {
+        expect(gpaCalc(1, 1, 1, 0, 0, 0).toEqual(0.000));
+    });
+
+    it('We should get 2.000 for A, C, F with 2 Credits each', function() {
+        expect(gpaCalc(2, 2, 2, 4, 2, 0).toEqual(0.000));
+    });
+
 });
