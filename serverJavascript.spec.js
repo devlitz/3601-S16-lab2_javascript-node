6c590d8241252596980ae8e62d0ab758b272d6a9
smil1201 <mill5978@morris.umn.edu>
2016-02-01 17:12:11 -0600

    updated gpaCalc test code.

--- serverJavascript.spec.js
+++ serverJavascript.spec.js
@@ -10,19 +10,19 @@ describe('testing stringLength', function(){
 //Testing for gpaCalc
 
 describe('testing gpaCalc', function(){
-    it('We should get 4.000 for all A', function() {
+    it('should return 4.000 for all A', function() {
         expect(gpaCalc(1, 1, 1, 4, 4, 4).toEqual(4.000));
     });
 
-    it('We should get 2.000 for all C', function() {
-        expect(gpaCalc(1, 1, 1, 2, 2, 2).toEqual(0.000));
+    it('should return 2.000 for all C', function() {
+        expect(gpaCalc(1, 1, 1, 2, 2, 2).not.toEqual(0.000));
     });
 
-    it('We should get 0.000 for all F', function() {
+    it('should return 0.000 for all F', function() {
         expect(gpaCalc(1, 1, 1, 0, 0, 0).toEqual(0.000));
     });
 
-    it('We should get 2.000 for A, C, F with 2 Credits each', function() {
+    it('should return 2.000 for A, C, F with 2 Credits each', function() {
         expect(gpaCalc(2, 2, 2, 4, 2, 0).toEqual(2.000));
     });
 
